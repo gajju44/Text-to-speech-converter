6af015bc7d78b8622fd4949114b6a360419ae017
Gajendra Naphade <118757883+gajju44@users.noreply.github.com>
2023-12-31 15:10:03 -0500

    Update project.py

--- project.py
+++ project.py
@@ -1,3 +1,25 @@
+"""
+GNU GENERAL PUBLIC LICENSE
+Version 3, 29 June 2007
+Copyright (C) [2022] [Gajendra Naphade]
+Copyright (C) [2022] [Piyush Pise]
+Copyright (C) [2022] [Harsh Dubey]
+Copyright (C) [2022] [Aryan Moon]
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
 import tkinter as tk
 from tkinter import *
 from PIL import Image,ImageTk
